7ad93934e02b76ebd516ee7dcd731b5402305c88
zanebennion <zbennion@nexusdatasolutions.com>
2026-07-28 10:18:40 -0800

    Fixed linting errors

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,36 +2,48 @@
   <div>
     <h1>FISH</h1>
     <form @submit="handlePress">
-      <input type="text" v-model="msg" placeholder="Enter message..." />
-      <UButton type="submit" color="primary"> Submit </UButton>
+      <input
+        v-model="msg"
+        type="text"
+        placeholder="Enter message..."
+      >
+      <UButton
+        type="submit"
+        color="primary"
+      >
+        Submit
+      </UButton>
     </form>
-    <p v-if="serverResponse">{serverResponse}</p>
+    <p v-if="serverResponse">
+      {serverResponse}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const msg = ref("");
-const serverResponse = ref("");
+const msg = ref('')
+const serverResponse = ref('')
 
 const handlePress = async () => {
-  if (!msg.value) return;
+  if (!msg.value) return
 
   try {
-    const response = await $fetch("/api/echo", {
-      method: "POST",
+    const response = await $fetch('/api/echo', {
+      method: 'POST',
       body: {
-        text: msg.value,
-      },
-    });
+        text: msg.value
+      }
+    })
 
-    serverResponse.value = response.msg;
-    msg.value = "";
-  } catch (error) {
-    console.error("API error: ", error);
+    serverResponse.value = response.msg
+    msg.value = ''
   }
-};
+  catch (error) {
+    console.error('API error: ', error)
+  }
+}
 </script>
 
 <style scoped></style>
--- server/api/echo.post.ts
+++ server/api/echo.post.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+  const body = await readBody(event)
 
-  console.log(body.text);
+  console.log(body.text)
 
   return {
     success: true,
-    msg: "Message sent",
-  };
-});
+    msg: 'Message sent'
+  }
+})
