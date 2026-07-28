3b17ddf75c3bf3312f8be55ebd7a7c046bfd644d
zanebennion <zbennion@nexusdatasolutions.com>
2026-07-28 09:26:27 -0800

    build for clouflare worker, not pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', '@vite-pwa/nuxt'],
+  modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/ui", "@vite-pwa/nuxt"],
 
   // https://devtools.nuxt.com
   devtools: { enabled: true },
+
+  // Build for Cloudflare workers, not pages
+  nitro: {
+    preset: "cloudflare-module",
+  },
 
   // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
   runtimeConfig: {
     public: {
       // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋'
-    }
+      helloText: "Hello from the Edge 👋",
+    },
   },
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
-  compatibilityDate: '2025-03-01',
+  compatibilityDate: "2025-03-01",
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
-    database: true
+    database: true,
   },
 
   // Development config
   eslint: {
     config: {
       stylistic: {
-        quotes: 'single',
-        commaDangle: 'never'
-      }
-    }
+        quotes: "single",
+        commaDangle: "never",
+      },
+    },
   },
 
   pwa: {
-    registerType: 'autoUpdate',
+    registerType: "autoUpdate",
     manifest: {
-      name: 'Sportfish Survey',
-      short_name: 'Sportfish',
-      theme_color: '#000000'
+      name: "Sportfish Survey",
+      short_name: "Sportfish",
+      theme_color: "#000000",
     },
     workbox: {
-      navigateFallback: '/'
-    }
-  }
-})
+      navigateFallback: "/",
+    },
+  },
+});
